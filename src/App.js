245befe1dec461d44ace87cd10fd30eff0d1f98e
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2022-10-25 00:12:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+import {useState} from 'react';
+import {decrement, increment, incrementByAmount} from "./redux/Counter"
+
 
 function App() {
+  const [number, setNumber] = useState('0');
+  const {count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementValue = Number(number) || 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> The Count is: {count}</h1>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+        
+          <input  value={number} onChange={(e) => setNumber(e.target.value)}/>
+    
+        <button onClick={() => dispatch(incrementByAmount(incrementValue))}>increment by amount</button>
     </div>
   );
 }
